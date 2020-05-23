e66cf7e3b41dfcb8a765e764866c242223ac6f2d
AlfredoPazZamora <46512729+AlfredoPazZamora@users.noreply.github.com>
2020-05-22 22:25:10 -0500

    Update pilas_aritmetica.js
    Agregamos el metodo push

--- pilas_aritmetica.js
+++ pilas_aritmetica.js
@@ -1,7 +1,14 @@
 class pila{
     constructor(data){
-        this.data = data;
-        this.next = null;
+        this.data = {};
+        this.count = 0;
     }
+
+    push(info){
+        this.data[this.count] = element;
+        this.count++;
+        return this.data;
+    }
+    
 }
 
