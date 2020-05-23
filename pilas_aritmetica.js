8ccd24b2db6247cb7fb710a887acd61ebe70753b
AlfredoPazZamora <46512729+AlfredoPazZamora@users.noreply.github.com>
2020-05-22 22:20:09 -0500

    Update pilas_aritmetica.js
    Agregamos el linkedlist para este ejemplo

---
+++ pilas_aritmetica.js
@@ -0,0 +1,13 @@
+class pila{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class linkedList{
+    constructor(){
+        this.head = null;
+        this.count = 0;
+    }
+}
